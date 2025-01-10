66ec762430c61a0993fef16d182877f34723a89c
Daniel Mikes <max8@post.cz>
2025-01-10 22:15:02 +0100

    script

--- script/docker-compose-run.sh
+++ script/docker-compose-run.sh
@@ -3,7 +3,10 @@
 source .env
 
 ssh $PI_USER/$PI_HOST << EOF
-  cd docker
-  source .venv/bin/activate
-  docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d $DOMAIN --email $MAIL --agree-tos --no-eff-email
+  cd ~/docker || exit
+
+  if ! docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot/certbot -d $DOMAIN --email $MAIL --agree-tos --no-eff-email; then
+    echo "CertBot failed to run"
+    exit 1
+  fi
 EOF
--- script/service-start.sh
+++ script/service-start.sh
@@ -4,8 +4,8 @@ source .env
 
 ssh $PI_USER@$PI_HOST << EOF
   sudo systemctl daemon-reload
-  sudo sytemctl enable containerd
+  sudo systemctl enable containerd
   sudo systemctl start containerd
-  sudo sytemctl enable docker
+  sudo systemctl enable docker
   sudo systemctl start docker
 EOF
